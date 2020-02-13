b8c6bb212f6e28e866e26b819b34de687b7c255c
Anastasiia H <anastasia.bq@gmail.com>
2020-02-13 16:36:15 -0500

    made page content dynamic

--- pages/views.py
+++ pages/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from listings.models import Listing
+from realtors.models import Realtor
+
 def index(request):
-    return render(request, 'pages/index.html')
+    #Get all listings
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3] #shows only 3 listings
+    context = {
+        'listings' : listings
+    }
+        
+    return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    #Get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    #Get mvp 
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+    context={
+        'realtors' : realtors,
+        'mvp_realtors' : mvp_realtors
+    }
+    return render(request, 'pages/about.html', context)
